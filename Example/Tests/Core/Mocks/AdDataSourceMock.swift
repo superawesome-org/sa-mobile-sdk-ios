062c2d79a7b892399c84bba00512af0039c86fb8
Gunhan Sancar <gunhan.sancar@superawesome.com>
2020-04-24 15:23:59 +0100

    Fixed unit tests

---
+++ Example/Tests/Core/Mocks/AdDataSourceMock.swift
@@ -0,0 +1,8 @@
+//
+//  AdDataSourceMock.swift
+//  Tests
+//
+//  Created by Gunhan Sancar on 24/04/2020.
+//
+
+import Foundation
---
+++ Pod/Plugin/Core/Extensions/Result+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Result+Extensions.swift
+//  SuperAwesome
+//
+//  Created by Gunhan Sancar on 24/04/2020.
+//
+
+import Foundation
