5658e1c900a2c8fc18ba34f4643d7e1ea15e9c1d
mark gilchrist <mark.s.w.gilchrist@gmail.com>
2021-06-02 14:11:36 +0100

    update swifl lint errros and pod update

--- Example/Tests/SAIsMinor/TestsUtils.swift
+++ Example/Tests/SAIsMinor/TestsUtils.swift
@@ -8,43 +8,40 @@
 
 import Foundation
 
-import Foundation
-// import SAGDPRKisMinor
-
 enum FixtureError: Error {
-    case CannotLoadMainBundle
-    case CannotLoadFixturesBundle
-    case CannotLoadFixture
-    case CannotLoadData
-    case CannotParseData
+    case cannotLoadMainBundle
+    case cannotLoadFixturesBundle
+    case cannotLoadFixture
+    case cannotLoadData
+    case cannotParseData
 }
 
 func fixtureWithName(name: String, ofType: String = "json") throws -> AnyObject {
 
     guard let testBundle = Bundle(identifier: "org.cocoapods.demo.SAGDPRKisMinor-Tests") else {
-        print(FixtureError.CannotLoadMainBundle)
-        throw FixtureError.CannotLoadMainBundle
+        print(FixtureError.cannotLoadMainBundle)
+        throw FixtureError.cannotLoadMainBundle
     }
     guard let fixturesBundlePath = testBundle.path(forResource: "fixtures", ofType: "bundle") else {
-        print(FixtureError.CannotLoadFixturesBundle)
-        throw FixtureError.CannotLoadFixturesBundle
+        print(FixtureError.cannotLoadFixturesBundle)
+        throw FixtureError.cannotLoadFixturesBundle
     }
     guard let fixturesBundle = Bundle(path: fixturesBundlePath) else {
-        print(FixtureError.CannotLoadFixturesBundle)
-        throw FixtureError.CannotLoadFixturesBundle
+        print(FixtureError.cannotLoadFixturesBundle)
+        throw FixtureError.cannotLoadFixturesBundle
     }
     guard let path = fixturesBundle.path(forResource: name, ofType: ofType) else {
-        print(FixtureError.CannotLoadFixture)
-        throw FixtureError.CannotLoadFixture
+        print(FixtureError.cannotLoadFixture)
+        throw FixtureError.cannotLoadFixture
     }
     guard let data = NSData(contentsOfFile: path) else {
-        print(FixtureError.CannotLoadData)
-        throw FixtureError.CannotLoadData
+        print(FixtureError.cannotLoadData)
+        throw FixtureError.cannotLoadData
     }
 
     do {
         try JSONSerialization.jsonObject(with: data as Data, options: .allowFragments)
-    } catch (let value) {
+    } catch let value {
         print(value)
     }
 
