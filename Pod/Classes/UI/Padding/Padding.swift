73832bc141401828e6dfb0134b81526c6574dd76
Tom <tom.orourke@superawesome.com>
2022-06-28 12:01:04 +0100

    Removed unused padding cases

--- Pod/Classes/UI/Padding/Padding.swift
+++ Pod/Classes/UI/Padding/Padding.swift
@@ -8,17 +8,7 @@
 import UIKit
 
 internal enum Padding: CGFloat {
-    case xxsx = 1.0
-    case xxs = 2.0
-    case xsx = 3.0
-    case xs = 4.0
-    case sx = 6.0
     case s = 8.0
-    case sm = 12.0
-    case m = 16.0
-    case l = 32.0
-    case xl = 64.0
-    case xxl = 128.0
 
     public var value: CGFloat {
         rawValue
