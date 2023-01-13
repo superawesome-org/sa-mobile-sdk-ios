85f10420dadac99fe7ff7426fbfb8b26f7e5df82
Tom O'Rourke <30452349+thomas-james-1986@users.noreply.github.com>
2023-01-13 11:38:53 +0000

    Feat/aag 2717 move placements to firebase (#227)
    * chore(): Modified the example app to load the placements from Firebase
    
    * chore(): update main view id
    
    * chore(): UI testing updates and fixes
    
    * chore(): CI fixes for Firebase
    
    * chore(): swiftlint changes
    
    * chore(): fix context
    
    * chore(): CI context fix
    
    * chore(): ci context fix
    
    * chore(): CI path fix
    
    * fix(): CI decode fix
    
    * chore(): SwiftLint fix
    
    * chore(): Log fix
    
    * chore(): Removed unused stackview
    
    * chore(): cleanup

---
+++ Example/SuperAwesome/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SuperAwesomeExample
+//
+//  Created by Tom O'Rourke on 11/01/2023.
+//
+
+import Foundation
+
+struct Constants {
+    static let firebaseDatabaseURL = "https://awesomeads-default-rtdb.europe-west1.firebasedatabase.app/"
+}
