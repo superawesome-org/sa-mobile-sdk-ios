eb4fda15cb498fe8519a07576f916d1a803abb90
Gunhan Sancar <gunhan.sancar@superawesome.com>
2023-01-20 12:03:17 +0000

    chore(): update podspec

--- Adapters/AdMob/Classes/SAAdMobAdapter.swift
+++ Adapters/AdMob/Classes/SAAdMobAdapter.swift
@@ -10,13 +10,13 @@ import SuperAwesome
 
 @objc(SAAdMobAdapter)
 class SAAdMobAdapter: NSObject, GADMediationAdapter {
-    
-    fileprivate var bannerAd: SAAdMobBannerAd? = nil
-    
+
+    fileprivate var bannerAd: SAAdMobBannerAd?
+
     required override init() {
         super.init()
     }
-    
+
     static func adapterVersion() -> GADVersionNumber {
         let versionNumber = AwesomeAds.info()?.versionNumber ?? ""
         let versionComponents = versionNumber.components(separatedBy: ".")
@@ -28,37 +28,37 @@ class SAAdMobAdapter: NSObject, GADMediationAdapter {
         }
         return version
     }
-    
+
     static func adSDKVersion() -> GADVersionNumber {
         let versionNumber = AwesomeAds.info()?.versionNumber ?? ""
         let versionComponents = versionNumber.components(separatedBy: ".")
-        
+
         if versionComponents.count >= 3 {
             let majorVersion = Int(versionComponents[0]) ?? 0
             let minorVersion = Int(versionComponents[1]) ?? 0
             let patchVersion = Int(versionComponents[2]) ?? 0
-            
+
             return GADVersionNumber(majorVersion: majorVersion,
                                     minorVersion: minorVersion,
                                     patchVersion: patchVersion)
         }
-        
+
         return GADVersionNumber()
     }
-    
+
     static func networkExtrasClass() -> GADAdNetworkExtras.Type? {
        nil
     }
-    
+
     func loadBanner(for adConfiguration: GADMediationBannerAdConfiguration, completionHandler: @escaping GADMediationBannerLoadCompletionHandler) {
         bannerAd = SAAdMobBannerAd()
         bannerAd?.loadBanner(for: adConfiguration, completionHandler: completionHandler)
     }
-    
+
     func loadInterstitial(for adConfiguration: GADMediationInterstitialAdConfiguration, completionHandler: @escaping GADMediationInterstitialLoadCompletionHandler) {
         // TODO: Load interstitial
     }
-    
+
     func loadRewardedAd(for adConfiguration: GADMediationRewardedAdConfiguration, completionHandler: @escaping GADMediationRewardedLoadCompletionHandler) {
         // TODO: Load video ad
     }
