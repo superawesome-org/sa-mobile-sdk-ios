645da8da22362d0f161072edd5d2235903489941
Tom O'Rourke <thomasjamesorourke@hotmail.co.uk>
2023-02-28 19:47:01 +0000

    chore(): Added a helper to get a CloseButtonState from an int

--- Pod/Classes/Common/State/CloseButtonState.swift
+++ Pod/Classes/Common/State/CloseButtonState.swift
@@ -5,8 +5,18 @@
 //  Created by Tom O'Rourke on 08/06/2022.
 //
 
-enum CloseButtonState {
-    case visibleWithDelay
-    case visibleImmediately
-    case hidden
+@objc
+public enum CloseButtonState: Int {
+    case visibleWithDelay = 0
+    case visibleImmediately = 1
+    case hidden = 2
+}
+
+@objc
+public class CloseButtonStateHelper: NSObject {
+    /// Creates a `CloseButtonState` enum from an `int` value.
+    @objc
+    public class func from(_ value: Int) -> CloseButtonState {
+        CloseButtonState(rawValue: value) ?? Constants.defaultCloseButton
+    }
 }
