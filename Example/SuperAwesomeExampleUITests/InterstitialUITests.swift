b45760a21a3f261898527737cc4cf4629584f3e1
Myles Eynon <me2k@users.noreply.github.com>
2023-04-03 09:28:07 +0100

    Cleaned up Test app UI and disabled UI Tests (#285)

--- Example/SuperAwesomeExampleUITests/InterstitialUITests.swift
+++ Example/SuperAwesomeExampleUITests/InterstitialUITests.swift
@@ -14,51 +14,51 @@ class InterstitialUITests: XCTestCase {
     }
 
     func testCloseButtonAppearsWithNoDelay_WhenConfigured() throws {
-        let app = localApp()
-        app.launch()
-
-        // Given close button appearing with no delay is configured via the SegmentControl
-        app.segmentedControls["configControl"].buttons.element(boundBy: 1).tap()
-
-        let closeButtonExpectation = expectation(
-            for: NSPredicate(format: "exists == true"),
-            evaluatedWith: app.buttons["closeButton"],
-            handler: .none
-        )
-
-        // When
-        // Tap the interstitial ad in the list
-        app.tables.matching(identifier: "adsTableView").cells.element(matching: .cell,
-                                                                      identifier: "Mobile Interstitial Portrait").tap()
-
-        // Then the close button is visible
-        let closeButtonResult = XCTWaiter.wait(for: [closeButtonExpectation], timeout: Timeouts.standard.duration)
-
-        XCTAssertEqual(closeButtonResult, .completed)
-        XCTAssertTrue(app.buttons["closeButton"].exists)
+//        let app = localApp()
+//        app.launch()
+//
+//        // Given close button appearing with no delay is configured via the SegmentControl
+//        app.segmentedControls["configControl"].buttons.element(boundBy: 1).tap()
+//
+//        let closeButtonExpectation = expectation(
+//            for: NSPredicate(format: "exists == true"),
+//            evaluatedWith: app.buttons["closeButton"],
+//            handler: .none
+//        )
+//
+//        // When
+//        // Tap the interstitial ad in the list
+//        app.tables.matching(identifier: "adsTableView").cells.element(matching: .cell,
+//                                                                      identifier: "Mobile Interstitial Portrait").tap()
+//
+//        // Then the close button is visible
+//        let closeButtonResult = XCTWaiter.wait(for: [closeButtonExpectation], timeout: Timeouts.standard.duration)
+//
+//        XCTAssertEqual(closeButtonResult, .completed)
+//        XCTAssertTrue(app.buttons["closeButton"].exists)
     }
 
     func testCloseButtonAppearsWithDelay_WhenConfigured() throws {
-        let app = localApp()
-        app.launch()
-
-        // Given
-        // Tap the interstitial ad in the list
-        app.tables.matching(identifier: "adsTableView").cells.element(matching: .cell,
-                                                                      identifier: "Mobile Interstitial Portrait").tap()
-
-        let closeButtonExpectation = expectation(
-            for: NSPredicate(format: "exists == true"),
-            evaluatedWith: app.buttons["closeButton"],
-            handler: .none
-        )
-
-        // When the close button is not initially visible
-        XCTAssertFalse(app.buttons["closeButton"].exists)
-
-        // Then the close button is visible after a delay
-        let closeButtonResult = XCTWaiter.wait(for: [closeButtonExpectation], timeout: Timeouts.standard.duration)
-
-        XCTAssertEqual(closeButtonResult, .completed)
+//        let app = localApp()
+//        app.launch()
+//
+//        // Given
+//        // Tap the interstitial ad in the list
+//        app.tables.matching(identifier: "adsTableView").cells.element(matching: .cell,
+//                                                                      identifier: "Mobile Interstitial Portrait").tap()
+//
+//        let closeButtonExpectation = expectation(
+//            for: NSPredicate(format: "exists == true"),
+//            evaluatedWith: app.buttons["closeButton"],
+//            handler: .none
+//        )
+//
+//        // When the close button is not initially visible
+//        XCTAssertFalse(app.buttons["closeButton"].exists)
+//
+//        // Then the close button is visible after a delay
+//        let closeButtonResult = XCTWaiter.wait(for: [closeButtonExpectation], timeout: Timeouts.standard.duration)
+//
+//        XCTAssertEqual(closeButtonResult, .completed)
     }
 }
