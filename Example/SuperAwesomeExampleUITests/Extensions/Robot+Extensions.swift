788f5ffe0104bff70dde8327e727ce0a65d89feb
Tom O'Rourke <thomasjamesorourke@hotmail.co.uk>
2023-04-19 17:31:36 +0100

    chore(): documenting colour testing method

--- Example/SuperAwesomeExampleUITests/Extensions/Robot+Extensions.swift
+++ Example/SuperAwesomeExampleUITests/Extensions/Robot+Extensions.swift
@@ -5,18 +5,49 @@
 //  Created by Tom O'Rourke on 18/04/2023.
 //
 
-import Foundation
 import XCTest
 import DominantColor
 
 extension Robot {
     
-    func AssertExpectedColor(expectedColor: String, sampleSize: CGFloat, image: UIImage) {
+    /**
+     * Method that asserts that a sampled colour of an image is the expected colour
+     *
+     * - Parameters:
+     *  - expectedColor: The expected colour as a hex string e.g: "#FFFFFF"
+     *  - sampleSize: The number of pixels to sample from the centre of the image
+     *  - image: The image to test, e.g: `closeButton.screenshot().image`
+     */
+    func AssertExpectedColor(
+        expectedColor: String,
+        sampleSize: CGFloat,
+        image: UIImage,
+        file: StaticString = #file,
+        line: UInt = #line)
+    {
         
-        let crop = image.centreCroppedTo(CGSize(width: sampleSize, height: sampleSize))
-        let sampledColour = crop.dominantColors().first(where: { $0.hexString() == expectedColor })
+        guard sampleSize > 2 else {
+            XCTFail("The sample size must be greater than 2", file: file, line: line)
+            return
+        }
         
-        XCTAssertEqual(expectedColor, sampledColour?.hexString())
+        let crop = image.centreCroppedTo(
+            CGSize(
+                width: sampleSize,
+                height: sampleSize
+            )
+        )
+        
+        let sampledColour = crop.dominantColors().first(
+            where: { $0.hexString() == expectedColor }
+        )
+        
+        XCTAssertEqual(
+            expectedColor,
+            sampledColour?.hexString(),
+            file: file,
+            line: line
+        )
     }
     
     func AssertExpectedScreenshotColor(expectedColor: String) {
